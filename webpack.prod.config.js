963ff515552514c65426e5e55d68191480798dbc
Andela Developer <tawakalt.olaniyi@andela.com>
2017-06-21 12:38:02 +0100

    fix eslint errors

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,4 +1,4 @@
-const path = require("path");
+const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('webpack');
 const env = require('node-env-file');
@@ -7,15 +7,15 @@ const extractPlugin = new ExtractTextPlugin({
   filename: 'main.css'
 });
 
-const DIST_DIR = path.resolve(__dirname, "dist");
-const SRC_DIR = path.resolve(__dirname, "src");
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const SRC_DIR = path.resolve(__dirname, 'src');
 
 const config = {
-  entry: SRC_DIR + "/app/index.jsx",
+  entry: SRC_DIR + '/app/index.jsx',
   output: {
-    path: DIST_DIR + "/app",
-    filename: "bundle.js",
-    publicPath: "/app/"
+    path: DIST_DIR + '/app',
+    filename: 'bundle.js',
+    publicPath: '/app/'
   },
   externals: {
     'cheerio': 'window',
@@ -27,10 +27,10 @@ const config = {
       {
         test: /\.jsx?/,
         include: SRC_DIR,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ["react", "es2015", "stage-2"]
+          presets: ['react', 'es2015', 'stage-2']
         }
       },
 
@@ -55,4 +55,3 @@ const config = {
 };
 
 module.exports = config;
-  
