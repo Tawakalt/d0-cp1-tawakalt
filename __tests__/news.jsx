8e8010a5275676edf0b57de4ce1f7bd4befd1ce4
Andela Developer <tawakalt.olaniyi@andela.com>
2017-06-22 15:32:51 +0100

    modify news test

--- __tests__/news.jsx
+++ __tests__/news.jsx
@@ -8,12 +8,14 @@ import Utils from '../src/app/utils';
 describe('News', () => {
 
   const wrapper = mount(<News />);
-  const updateSearch2 = wrapper.instance().updateSearch2();
+  const createScrapeUrl = wrapper.instance().createScrapeUrl();
+  const createUrl = wrapper.instance().createUrl();
   const handleOpenModal = wrapper.instance().handleOpenModal();
   const handleCloseModal = wrapper.instance().handleCloseModal();
   const search = wrapper.instance().search();
   const sources = wrapper.instance().sources();
   const scrape = wrapper.instance().scrape();
+  const getSort = wrapper.instance().getSort();
   let MockRequest;
 
   beforeEach(() => {
@@ -41,4 +43,14 @@ test('calls method scrape', () => {
     expect(search).toEqual(undefined);
   });
 
+  test('onSelect selectSearch and onClick button should call updateSearch function', () => {
+    wrapper.find('#selectSearch').simulate('select');
+    wrapper.find('.createUrl').simulate('click');
+    expect(createUrl).toEqual(undefined);
+  });
+
+  test('onSelect sources, function getSort should be called', () => {
+    wrapper.find('#selectSources').simulate('select');
+    expect(getSort).toEqual(undefined);
+  });
 });
